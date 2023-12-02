3dcffc79309551b16d99ac5a372786642cd58e55
Sonya <sonya@gmail.com>
2023-12-02 15:39:48 +0530

    METHOD TEST ADDED BY SONYA

---
+++ src/JavaPracs/MethodTest.java
@@ -0,0 +1,21 @@
+package JavaPracs;
+
+public class MethodTest {
+	public int add (int a, int b)
+	{
+		return a+b;
+	}
+	public int sub (int a, int b)
+	{
+		return a-b;
+	}
+	public void Result_1()
+	{
+		System.out.println("Result 1");
+	}
+	public void Result_2()
+	{
+		System.out.println("Result 2");
+	}
+	
+}
