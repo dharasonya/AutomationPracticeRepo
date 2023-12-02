cd95f69e16fb6e570ebc18460a33f42b345e2b4e
Sonya <sonya@gmail.com>
2023-12-02 14:20:18 +0530

    New TestClass File added by Sonya

---
+++ src/JavaPracs/NewClassTest.java
@@ -0,0 +1,28 @@
+package JavaPracs;
+
+public class NewClassTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo Test");
+
+		String text="ttt";
+		int strLength=text.length()-1;
+		System.out.println(strLength);
+		
+		for(int i=0;i<=strLength;i++)
+		{
+			 if(text.charAt(i)=='a' || text.charAt(i)=='e')
+			 {
+				 System.out.println("Vowel");
+				 break;
+			 }
+			 else
+			 {
+				 continue;
+			 } 
+		}
+		
+	}
+
+}
